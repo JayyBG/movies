4a736d60534e826717f3ce7b1813c07fc4e74c0c
JayyBG <74960158+JayyBG@users.noreply.github.com>
2023-10-12 23:09:14 +0200

    Revert "Add files via upload"
    This reverts commit aec05dc69400a6c6009c882518b1da8b7fa2eea2.

--- dist/middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (req, res, next) => {
-    if (!process.env.ORIGIN) {
-        throw new Error("ORIGIN not set in .env file");
-    }
-    res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
-    next();
-};
